de53183c70df0d39c99a4d2d164022fd1e863a2c
Atercatus <myleelee2053@naver.com>
2020-08-05 18:48:37 +0900

    dump

--- src/components/arch/__tests__/main.test.ts
+++ src/components/arch/__tests__/main.test.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
-import {} from "@testing-library/jest-dom/matchers";
-import { screen } from "@testing-library/dom";
+// import {} from "@testing-library/jest-dom/matchers";
+// import { screen } from "@testing-library/dom";
 
 const html = fs.readFileSync(path.resolve(__dirname, "../arch.tmpl.js"));
 console.log(html.toString());
@@ -24,6 +24,7 @@ describe("type test", () => {
 
   it("dom", () => {
     // const div = getByRole(document.documentElement, "dialog");
-    const div = screen.getByRole("dialog");
+    // const div = screen.getByRole("dialog");
+    console.log(document.body.innerHTML);
   });
 });
